5e1847f000a4f876bd8527fc0142f0813312c71b
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-12 00:58:22 +0600

    Updated style sheet.

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const locations = [
     }
 ];
 
-const monster = [
+const monsters = [
     {name: 'slime', level: 2, health: 15},
     {name: 'fanged beast', level: 8, health: 60},
     {name: 'dragon', level: 20, health: 200}
@@ -59,6 +59,10 @@ const weapons = [
 btn1.onclick = goStore;
 btn2.onclick = goCave;
 btn1.onclick = fightDragon;
+
+function update() {
+    
+}
 
 function goStore() {
 
