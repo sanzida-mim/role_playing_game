9eabb1e7a217010e50e1d8bda8a90bbcceb2e601
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-11 23:31:44 +0600

    Getting started with gaming functionalities in JS.

---
+++ script.js
@@ -0,0 +1,7 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+
+const butnSt = document.querySelector('#button1');
+const butnCv = document.querySelector('#button2');
+const butnFd = document.querySelector('#button3');
