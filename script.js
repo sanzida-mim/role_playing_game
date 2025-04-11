eaae6f356523133248a5c1374f6eedd22e4d19ab
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-12 00:13:23 +0600

    Created starter function for store, cave, dragon, town, & monsters.

--- script.js
+++ script.js
@@ -9,3 +9,81 @@ const btn3 = document.querySelector('#button3');
 const xpText = document.querySelector('#xpText');
 const healthText = document.querySelector('#healthText');
 const goldText = document.querySelector('#goldText');
+
+const locations = [
+    {
+        name: 'town square',
+        'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+        'button function': [goStore, goCave, fightDragon],
+        text: 'You are in the town square. You see a sign that says \"Store\".'
+    },
+
+    {
+        name: 'store',
+        'button text': ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
+        'button function': [buyHealth, buyWeapon, goTown],
+        text: 'You enter the store.'
+    },
+
+    {
+        name: 'cave',
+        'button text': ['Fight Slime', 'Fight fanged beast', 'Go to town square'],
+        'button function': [fightSlime, fightFangedBeast, goTown],
+        text: 'You enter the cave. You see some monsters.'
+    },
+
+    {
+        name: 'fight dragon',
+        'button text': ['Attack', 'Dodge', 'Run'],
+        'button function': [attack, dodge, run],
+        text: 'You are fighting a monster.'
+    }
+];
+
+btn1.onclick = goStore;
+btn2.onclick = goCave;
+btn1.onclick = fightDragon;
+
+function goStore() {
+
+}
+
+function goCave() {
+
+}
+
+function fightDragon() {
+
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
+function goTown() {
+
+}
+
+function fightSlime() {
+
+}
+
+function fightFangedBeast() {
+
+}
+
+function attack() {
+
+}
+
+function dodge() {
+
+}
+
+function run() {
+
+}
